32e9ceb3196ddb5b141d16652fdb481fd46103c5
kimuJW <a01020532509@gmail.com>
2023-07-04 13:06:49 +0900

    codes

---
+++ day3.py
@@ -0,0 +1,15 @@
+t1 = ('banana', ' apple', 'kiwi')
+t1
+t2 = 'banana', 'apple', 'kiwi'
+t2
+t2[0] = 'watermelon'
+members = {'name':'안지영', 'age':30, 'email':'jiyoung@korea.com'}
+members
+members.keys()
+members.values()
+members.items()
+members.get('name')
+'name' in members
+'birth' in members
+members.clear()
+members
