1c22d87c22f79fc384f1f9274d724aba29eddfa9
kimuJW <a01020532509@gmail.com>
2023-07-26 18:59:52 +0900

    codes

---
+++ day25.py
@@ -0,0 +1,45 @@
+%matplotlib inline
+import matplotlib.font_manager as fm
+import matplotlib.pyplot as plt
+
+
+x=list(range(0,10))
+y1=[0,1,2,3,4,5,6,7,8,9]
+y2=[0,1,4,9,16,25,36,49,64,81]
+
+plt.figure()
+plt.plot(x,y1)
+plt.figure()
+plt.plot(x,y2)
+
+plt.show()
+
+plt.figure(figsize=(20,12))
+
+plt.subplot(221)
+plt.subplot(222)
+plt.subplot(212)
+
+plt.show()
+
+
+plt.figure(figsize=(20,12))
+
+plt.subplot(221)
+plt.plot([1,2,3], [110,130,120])
+plt.grid()
+
+plt.subplot(222)
+plt.plot(["월","화","수","목","금","토","일"], [28,30,29,31,32,31,31] )
+plt.xlabel('요일')
+plt.ylabel('기온')
+plt.title('최고기온')
+plt.grid()
+
+plt.subplot(212)
+y = [5, 3, 7, 10, 9, 5, 3.5, 8]
+x = range(len(y))
+plt.barh(x, y, height=0.7, color="red")
+plt.grid()
+
+plt.show()
